6126dc4a710c0fb204e870498da485354542a6e7
igor <igor@DESKTOP-55M0B0S>
2018-11-30 16:32:16 +0200

    commit 10

--- src/ua/com/foxminded/dao/StudentDAO.java
+++ src/ua/com/foxminded/dao/StudentDAO.java
@@ -5,49 +5,54 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import ua.com.foxminded.domain.Student;
 
 public class StudentDAO {
 
-    public Student create(String name) throws DAOException {
+    public Student create(Student student) throws DAOException {
         String sql = "INSERT INTO student (name) VALUES (?);";
-        Student student = null;
+        String name = student.getName();
+        Student studentCreated = null;
         try (Connection connection = DAOPostgreSQLConnection.getConnection();
                 PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);) {
             statement.setString(1, name);
             statement.execute();
             ResultSet rs = statement.getGeneratedKeys();
             rs.next();
-            student = new Student(rs.getString("name"));
-            student.setId(rs.getInt("id"));
+            studentCreated = new Student(rs.getString("name"));
+            studentCreated.setId(rs.getInt("id"));
         } catch (SQLException e) {
             throw new DAOException("Cannot create student", e);
         }
 
-        return student;
+        return studentCreated;
     }
 
-    public Student update(Integer id, String name, Integer group_id) throws DAOException {
+    public Student update(Student student, Integer group_id) throws DAOException {
         String sql = "UPDATE student SET name = ?, group_id = ? WHERE id = ?;";
-        Student student = null;
+        String name = student.getName();
+        Integer id = student.getId();
+        Student studentUpdated = null;
         try (Connection connection = DAOPostgreSQLConnection.getConnection();
                 PreparedStatement statement = connection.prepareStatement(sql);) {
             statement.setString(1, name);
             statement.setInt(2, group_id);
             statement.setInt(3, id);
             if (statement.executeUpdate() != 0) {
-                student = new Student(name);
-                student.setId(id);
+                studentUpdated = new Student(name);
+                studentUpdated.setId(id);
             }
         } catch (SQLException e) {
             throw new DAOException("Cannot update student", e);
         }
 
-        return student;
+        return studentUpdated;
     }
 
-    public Student read(Integer id) throws DAOException {
+    public Student findOne(Integer id) throws DAOException {
         String sql = "SELECT * FROM student WHERE id = ?;";
         Student student = null;
         try (Connection connection = DAOPostgreSQLConnection.getConnection();
@@ -60,10 +65,29 @@ public class StudentDAO {
                 student.setName(rs.getString("name"));
             }
         } catch (SQLException e) {
-            throw new DAOException("Cannot read student", e);
+            throw new DAOException("Cannot find student", e);
         }
 
         return student;
+    }
+
+    public List<Student> findAll() throws DAOException {
+        String sql = "SELECT * FROM student;";
+        List<Student> studentList = new ArrayList<>();
+        try (Connection connection = DAOPostgreSQLConnection.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql);) {
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                Student student = new Student();
+                student.setId(rs.getInt("id"));
+                student.setName(rs.getString("name"));
+                studentList.add(student);
+            }
+        } catch (SQLException e) {
+            throw new DAOException("Cannot find all students", e);
+        }
+
+        return studentList;
     }
 
     public Student delete(Integer id) throws DAOException {
