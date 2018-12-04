8e4f8703a1778fa99dd9c412f8310045fad5f934
igor <igor@DESKTOP-55M0B0S>
2018-12-04 23:43:38 +0200

    commit 11

--- src/ua/com/foxminded/dao/DAOPostgreSQLConnection.java
+++ src/ua/com/foxminded/dao/DAOPostgreSQLConnection.java
@@ -1,16 +1,32 @@
 package ua.com.foxminded.dao;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DAOPostgreSQLConnection {
-    private static String user = "postgres";
-    private static String password = "123456";
-    private static String url = "jdbc:postgresql://localhost:5432/university";
- 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(url, user, password);
+
+    public static Connection getConnection() throws DAOException {
+        String url = "";
+        String user = "";
+        String password = "";
+        try (FileInputStream fis = new FileInputStream("resources/dao.properties");) {
+            Properties prop = new Properties();
+            prop.load(fis);
+            url = prop.getProperty("url");
+            user = prop.getProperty("user");
+            password = prop.getProperty("password");
+        } catch (IOException e) {
+            throw new DAOException("Cannot find file properties", e);
+        }
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            throw new DAOException("Cannot get connection", e);
+        }
     }
 
 }
