f305388541e8aa207e4e2590a5c27cd9bd36c542
igor <igor@DESKTOP-55M0B0S>
2019-04-09 15:24:52 +0300

    commit 44

--- src/main/java/ua/com/foxminded/dao/ConnectionFactory.java
+++ src/main/java/ua/com/foxminded/dao/ConnectionFactory.java
@@ -3,19 +3,15 @@ package ua.com.foxminded.dao;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 public class ConnectionFactory {
-    private DataSource dataSource;
 
     public Connection getConnection() {
         try {
-            Context initCtx = new InitialContext();
-            Context envCtx = (Context) initCtx.lookup("java:comp/env");
-            dataSource = (DataSource) envCtx.lookup("jdbc/university");
+            DataSource dataSource = (DataSource) new InitialContext().lookup("java:/comp/env/jdbc/university");
             return dataSource.getConnection();
         } catch (SQLException e) {
             throw new DAOException("Cannot get connection", e);
